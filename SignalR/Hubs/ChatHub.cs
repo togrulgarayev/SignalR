3daad3e09d11ccca3d3dff3868a1a291c2e95fcc
Toğrul Qarazadə <garazade.tg@gmail.com>
2022-01-18 03:16:17 +0400

    Add project files.

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public Task SendMessageToGroup(string groupname, string sender, string message)
+        {
+            return Clients.Group(groupname).SendAsync("SendMessage", sender, message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+
+        
+    }
+}
